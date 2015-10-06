c4633e2c5c7eb811415b817536efc6915e34c749
anup <anupt6@gmail.com>
2015-10-06 17:36:46 +0530

    new project

---
+++ SpringSample/src/main/java/com/lsn/spring/Executable.java
@@ -0,0 +1,15 @@
+package com.lsn.spring;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Executable {
+	public static RegistrationBean registrationBean;
+	public static ClassPathXmlApplicationContext context;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+	}
+
+}
---
+++ SpringSample/src/main/java/com/lsn/spring/RegistrationBean.java
@@ -0,0 +1,19 @@
+package com.lsn.spring;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RegistrationBean {
+	@Autowired
+private UserRepository repository;
+	public RegistrationBean(){}
+	public UserRepository getRepository(){
+		return repository;
+	}
+	public void setRepository(UserRepository repository){
+		this.repository=repository;
+	}
+	}
+
+ 
